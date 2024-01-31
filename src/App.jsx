30bcdc5ec0ecd48c8c4cbf38230faee448f33f19
sadrilamin1999 <sadrilshuvo420@gmail.com>
2024-01-31 16:51:20 +0600

    game done

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import Bord from "./components/Bord";
 
 const App = () => {
   return (
-    <>
+    <div className="h-screen h-screen bg-bgColor-backgroundColor flex justify-center items-center">
       <Bord />
-    </>
+    </div>
   );
 };
 
--- src/components/Bord.jsx
+++ src/components/Bord.jsx
@@ -29,24 +29,28 @@ const Bord = () => {
     setXIsNext(!isXNext);
   };
   return (
-    <>
-      <div>{status}</div>
-      <div className="flex">
-        <Square value={squares[0]} onClickHandler={() => handleClick(0)} />
-        <Square value={squares[1]} onClickHandler={() => handleClick(1)} />
-        <Square value={squares[2]} onClickHandler={() => handleClick(2)} />
+    <div className=" bg-purple-700 p-12 rounded-xl flex flex-col items-center gap-5">
+      <div>
+        <h2 className=" text-rose-50 text-6xl">{status}</h2>
       </div>
-      <div className="flex">
-        <Square value={squares[3]} onClickHandler={() => handleClick(3)} />
-        <Square value={squares[4]} onClickHandler={() => handleClick(4)} />
-        <Square value={squares[5]} onClickHandler={() => handleClick(5)} />
+      <div className="">
+        <div className="flex">
+          <Square value={squares[0]} onClickHandler={() => handleClick(0)} />
+          <Square value={squares[1]} onClickHandler={() => handleClick(1)} />
+          <Square value={squares[2]} onClickHandler={() => handleClick(2)} />
+        </div>
+        <div className="flex">
+          <Square value={squares[3]} onClickHandler={() => handleClick(3)} />
+          <Square value={squares[4]} onClickHandler={() => handleClick(4)} />
+          <Square value={squares[5]} onClickHandler={() => handleClick(5)} />
+        </div>
+        <div className="flex">
+          <Square value={squares[6]} onClickHandler={() => handleClick(6)} />
+          <Square value={squares[7]} onClickHandler={() => handleClick(7)} />
+          <Square value={squares[8]} onClickHandler={() => handleClick(8)} />
+        </div>
       </div>
-      <div className="flex">
-        <Square value={squares[6]} onClickHandler={() => handleClick(6)} />
-        <Square value={squares[7]} onClickHandler={() => handleClick(7)} />
-        <Square value={squares[8]} onClickHandler={() => handleClick(8)} />
-      </div>
-    </>
+    </div>
   );
 };
 
